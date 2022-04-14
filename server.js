b10bb09f1403cdd93a227198b056145668c4095d
luis <bluerizabiz@gmail.com>
2022-04-14 13:49:43 -0500

    fixed issue

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const { animals } = require(`./data/animals.json`);
 const express = require('express');
 const PORT = process.env.PORT || 3001;
@@ -41,16 +43,44 @@ function filterByQuery(query, animalsArray){
     //return the filtered results
     return filteredResults
 }
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
+//if (process.env.NODE_ENV === "production") {
+ //   app.use(express.static("build"));
+ //   app.get("*", (req, res) => {
+  //    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  //  });
+//  }
 
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    //return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -73,9 +103,29 @@ app.get('/api/animals/:id', (req, res) => {
       res.json(result);
   });
 
-  app.post('/api/animals', (req, res) => {});
+  // parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+  app.post('/api/animals', (req, res) => {
+      //set id based on what the next index of the array will be
+      req.body.id = animals.length.toString();
+
+      //if any data in req.body is incorrect, send 400 error back
+      if(!validateAnimal(req.body)){
+          res.status(400).send('the animal is not properly formatted.');
+      } else {
+          //add animal to json file and animals array in this function
+          const animal =createNewAnimal(req.body,animals);
+          res.json(animal);
+      }
+  });
+ 
+     
 
 
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Api server now on port ${PORT}!`);
 });
+
